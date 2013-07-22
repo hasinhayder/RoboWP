34e5a468a63549ff30e54f609b6b0ce3ffe0d35c
Hasin Hayder <hasin@leevio.com>
2013-07-22 14:41:50 +0600

    Instructions

--- instructions.php
+++ instructions.php
@@ -4,6 +4,6 @@
 		<li>Create a page and assign "Homepage" template from the Page Attributes section on the right side</li>
 		<li>Go to Settings->Reading from admin panel, select "A Static Page" and set the page you just created from the "Front Page" dropdown  </li>
 		<li>You are almost done. Now log into your admin panel and start creating Sliders, Features, Team Members and Posts and assign those posts to this "Home Page" you created in step 1, from the "Connected Pages" section in the right sidebar.</li>
-		<li>Enjoy!</li>
+		<li>Enjoy the detailed documentation at <a target="_blank" href='http://bit.ly/14x7UVZ'>Robo Documentation</a></li>
 	</ol>
 </div>
